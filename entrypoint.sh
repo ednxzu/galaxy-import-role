0af3c6aa6ec3687c7278fa2d6b29f935e90a5a90
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-03-09 13:50:08 +0100

    production ready version

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,6 @@
 set -e
 command_string="ansible-galaxy role import"
 
-echo "Github repo owner: ${GITHUB_REPOSITORY_OWNER}"
-echo "Github repo name: ${GITHUB_REPOSITORY}"
-echo "galaxy_api_key: ${GALAXY_API_TOKEN}"
-
 if [ -n "${GALAXY_API_TOKEN}" ]; then
   command_string="$command_string --api-key ${GALAXY_API_TOKEN} ${GITHUB_REPOSITORY%/*} ${GITHUB_REPOSITORY#*/}"
 fi
