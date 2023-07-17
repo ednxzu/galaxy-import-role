d2d1318d91dd96dc5fa15b9863684cb3003e2fe4
Bertrand Lanson <bertrand.lanson@protonmail.com>
2023-07-17 23:53:58 +0200

    update path to ansible-galaxy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -e
-command_string="ansible-galaxy role import"
+command_string="/usr/local/bin/ansible-galaxy role import"
 
 if [ -n "${GALAXY_API_TOKEN}" ]; then
   command_string="$command_string --api-key ${GALAXY_API_TOKEN} ${GITHUB_REPOSITORY%/*} ${GITHUB_REPOSITORY#*/}"
