154a551f3d0292a98e79136fdb9901c1fad1bc34
Bertrand Lanson <bertrand.lanson@protonmail.com>
2024-02-05 19:55:21 +0100

    feat(input): add owner and repo input to allow import from other repositories

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #! /bin/sh
 set -e
-command_string="ansible-galaxy role import"
-
-if [ -n "${GALAXY_API_TOKEN}" ]; then
-  command_string="$command_string --api-key ${GALAXY_API_TOKEN} ${GITHUB_REPOSITORY%/*} ${GITHUB_REPOSITORY#*/}"
-fi
+ansible_galaxy_command_string="ansible-galaxy role import"
+command_string="$ansible_galaxy_command_string --api-key ${GALAXY_API_TOKEN} ${GALAXY_REPOSITORY_OWNER} ${GALAXY_REPOSITORY_NAME}"
 
 echo "Command: $command_string"
 eval "$command_string"
